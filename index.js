abf52ff0bd1a2076fb18aa47ac65b27512cc1022
Justin <justinconnors@hotmail.ca>
2023-03-25 16:46:38 -0300

    bug with shape selection, working on fix

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const inquirer = require('inquirer');
 
+const Rectangle = require('./lib/shapes.js');
+const Circle = require('./lib/shapes.js');
+const Square = require('./lib/shapes.js');
+
+// let rectangle = new Rectangle();
+// let circle = new Circle();
+// let square = new Square();
+
 // Function for selecting the shape of the SVG
 function shapeSelection() {
     inquirer
@@ -11,10 +19,47 @@ function shapeSelection() {
                 name: 'svgShapes'
             }
         ])
-        .then((res) => {
-            const svgShape = res.svgShapes;
-            console.log(svgShape);
+        .then((shape) => {
+            const svgShape = shape.svgShapes;
+            if (svgShape === 'Square') {
+                inquirer
+                    .prompt([
+                        {
+                            type: 'input',
+                            message: 'Enter one side size of the square',
+                            name: 'squareSide'
+                        }
+                    ])
+                    .then((size) => {
+                        const sideSize = size.squareSide;
+                        console.log(new Square(sideSize));
+                    })
+
+            } else if (svgShape === 'Rectangle') {
+                inquirer
+                    .prompt([
+                        {
+                            type: 'input',
+                            message: 'Enter a Width',
+                            name: 'rectangleWidth'
+                        },
+                        {
+                            type: 'input',
+                            message: 'Enter a Height',
+                            name: 'rectangleHeight'
+                        }
+                    ])
+                    .then((recSize) => {
+                        console.log(svgShape);
+                        const recWidth = recSize.rectangleWidth;
+                        const recHeight = recSize.rectangleHeight;
+                        console.log(new Rectangle(recWidth, recHeight));
+                    })
+            }
         })
 }
 
 shapeSelection();
+
+// const container = document.getElementById('#svg');
+// container.innerHTML = rectangleSvg + squareSvg + circleSvg;
