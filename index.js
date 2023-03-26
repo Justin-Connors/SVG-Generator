5b6718ca6a89790fad4c8659ab6ffe0970fb707c
Justin <justinconnors@hotmail.ca>
2023-03-26 14:14:23 -0300

    new classes with render now work with indexjs, need to actually do something with string now

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer');
 const prompt = inquirer.createPromptModule();
 //Imports
-const { Rectangle, Circle, Square } = require('./lib/shapes.js');
+const { Rectangle, Circle, Square, AllShapes } = require('./lib/shapes.js');
 
 // Function for selecting the shape of the SVG
 function shapeSelection() {
@@ -15,7 +15,7 @@ function shapeSelection() {
             },
             {
                 type: 'input',
-                message: 'Enter shape color (keyword or hexadecimal):',
+                message: 'Enter shape color',
                 name: 'shapeColor'
             },
             {
@@ -26,6 +26,8 @@ function shapeSelection() {
         ])
         .then((shape) => {
             const svgShape = shape.svgShapes;
+            const color = shape.shapeColor;
+            const text = shape.svgChars;
 
             if (shape.svgChars.length > 3) {
                 console.log('You cannot use more than 3 Characters');
@@ -42,8 +44,8 @@ function shapeSelection() {
                     ])
                     .then((size) => {
                         const sideSize = size.squareSide;
-                        const squ = new Square(sideSize);
-                        console.log(squ);
+                        const squ = new Square(sideSize, color, text);
+                        console.log(squ.render());
                     })
 
             } else if (svgShape === 'Rectangle') {
@@ -62,8 +64,8 @@ function shapeSelection() {
                     .then((recSize) => {
                         const recWidth = recSize.rectangleWidth;
                         const recHeight = recSize.rectangleHeight;
-                        const rec = new Rectangle(recWidth, recHeight);
-                        console.log(rec);
+                        const rec = new Rectangle(recWidth, recHeight, color, text);
+                        console.log(rec.render());
                     })
 
             } else if (svgShape === 'Circle') {
@@ -76,8 +78,8 @@ function shapeSelection() {
                     ])
                     .then((r) => {
                         const radius = r.circleRadius;
-                        const circ = new Circle(radius);
-                        console.log(circ);
+                        const circ = new Circle(radius, color, text);
+                        console.log(circ.render());
                     })
             }
         })
